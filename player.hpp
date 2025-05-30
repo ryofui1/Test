d50cc097bf5abc176ddecf3fcf924f17f265bee9
ryofui1 <sheep.ryo8258@gmail.com>
2025-05-31 07:32:45 +0900

    .. 削除

--- player.hpp
+++ player.hpp
@@ -5,7 +5,7 @@ class Player {
 public:
     sf::RectangleShape shape;
     //定数はここで宣言+定義,変数(計算に使う変わる数など)はここで宣言のみ 
-    // 説明 ..
+    // 説明
     float speed = 50.f;
     float jumpForce = -0.4;
     bool onGround;
