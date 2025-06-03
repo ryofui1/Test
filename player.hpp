0b447d8b8870273c8f437f2d55ef989976b40242
tybob8010 <tyobob8010@gmail.com>
2025-06-03 12:57:06 +0900

    田中参戦

--- player.hpp
+++ player.hpp
@@ -7,6 +7,7 @@ public:
     //定数はここで宣言+定義,変数(計算に使う変わる数など)はここで宣言のみ 
     //hppの変更はexeの再生成に寄与しない
     //なので、cppで一度何か変更してからF5すること
+    //分かったby田中
     float speed = 50.f;
     float jumpForce = -0.4;
     bool onGround;
